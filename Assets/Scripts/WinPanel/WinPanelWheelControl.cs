5af4e9e0ee31fb250bfdc66d571860c29e1887b5
Hiloliddin <38894285+hiloliddinj@users.noreply.github.com>
2023-03-22 18:53:21 +0500

    Win Screen Designed with scale and rotations

--- Assets/Scripts/WinPanel/WinPanelWheelControl.cs
+++ Assets/Scripts/WinPanel/WinPanelWheelControl.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WinPanelWheelControl : MonoBehaviour
 {
-    //TODO: Become bigger and smaller with speed and rotate!!!
+    //TODO: Become bigger and smaller with speed and rotate
 
-    void Start()
+    [SerializeField] private GameObject wheel;
+    [SerializeField] private TextMeshProUGUI prizeValue;
+
+    private Vector3 scaleChange;
+    private float _amount = -0.1f;
+
+    private void Awake()
     {
-        
+        scaleChange = new Vector3(_amount, _amount, _amount) * Time.deltaTime;
+        prizeValue.SetText("$123");
     }
 
-    
-    void Update()
+    private void Update()
     {
-        
+        Scale();
+        Rotate();
+    }
+
+    private void Scale()
+    {
+        transform.localScale += scaleChange;
+        if (transform.localScale.y < 0.5f || transform.localScale.y > 1.5f)
+        {
+            scaleChange = -scaleChange;
+        }
+    }
+
+    private void Rotate()
+    {
+        wheel.transform.Rotate(0, 0, 30 * Time.deltaTime);
     }
 }
